f851367b5d6587fa85b80e0517a7ac54c370288b
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-02-13 17:57:24 +0100

    .

--- src/app/dhikr/dhikr-template/dhikr-template.component.ts
+++ src/app/dhikr/dhikr-template/dhikr-template.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { IonSlides } from '@ionic/angular';
 import { MediaPlayerService } from 'src/app/shared/media-player.service';
 import { NativePluginsService } from 'src/app/shared/native-plugins.service';
-import { DhikrService } from '../dhikr.service';
+import { DhikrLocalStoarge, DhikrService } from '../dhikr.service';
 @Component({
   selector: 'app-dhikr-template',
   templateUrl: './dhikr-template.component.html',
@@ -12,16 +12,14 @@ export class DhikrTemplateComponent implements OnInit {
   @Input() name: string;
   @ViewChild('slides', { static: true }) slides: IonSlides;
   @Input() dhikrs: any;
-  showBosnianDhikr = true;
-  showArabicDhikr = true;
-  showDhikrTranslation = false;
   currentTime = -0.5;
   startSecond: number;
   endSecond: number;
   sliderLength: number;
   sliderActiveIndex = 0;
+  dhikrLocalStoarge = DhikrLocalStoarge;
   constructor(
-    private dhikrService: DhikrService,
+    public dhikrService: DhikrService,
     private nativePluginsService: NativePluginsService,
     public mediaPlayerService: MediaPlayerService
   ) {}
@@ -37,20 +35,6 @@ export class DhikrTemplateComponent implements OnInit {
       this.setSliderLength();
     });
   }
-
-  // playAudio(audioUrl) {
-  //   this.mediaPlayerService.playAudio(audioUrl);
-  //   this.mediaPlayerService.watchCurrentTime();
-  // }
-
-  // stopAudioPlayer() {
-  //   this.mediaPlayerService.stopAudio();
-  // }
-
-  // setAudioCurrentTime(time, audioUrl){
-  //   this.mediaPlayerService.setAudioCurrentTime(time, audioUrl);
-  //   this.mediaPlayerService.watchCurrentTime();
-  // }
 
   getSliderActiveIndex() {
     this.slides.getActiveIndex().then((index: number) => {
--- src/app/dhikr/dhikr.service.ts
+++ src/app/dhikr/dhikr.service.ts
@@ -2,15 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
+export enum DhikrLocalStoarge {
+  'showArabicDhikr',
+  'showBosnianDhikr',
+  'showDhikrTranslation',
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DhikrService {
   morningDhikrPageEntered: Subject<boolean>;
   eveningDhikrPageEntered: Subject<boolean>;
+  showArabicDhikr: boolean;
+  showBosnianDhikr: boolean;
+  showDhikrTranslation: boolean;
+
   constructor(private http: HttpClient) {
     this.morningDhikrPageEntered = new Subject();
     this.eveningDhikrPageEntered = new Subject();
+    this.getItemsFromLocalStoarge();
    }
 
   getMorningDhikr() {
@@ -19,5 +29,44 @@ export class DhikrService {
 
   getEveningDhikr() {
     return this.http.get('assets/db/evening-dhikr.json');
+  }
+
+  getItemsFromLocalStoarge() {
+    const showArabicDhikrFromStorage = localStorage.getItem(DhikrLocalStoarge.showArabicDhikr.toString());
+    const showBosnianDhikrFromStorage = localStorage.getItem(DhikrLocalStoarge.showBosnianDhikr.toString());
+    const showDhikrTranslationFromStorage = localStorage.getItem(DhikrLocalStoarge.showDhikrTranslation.toString());
+
+    if(showArabicDhikrFromStorage){
+      this.showArabicDhikr = JSON.parse(showArabicDhikrFromStorage);
+    }
+    else {
+      this.showArabicDhikr = true;
+    }
+
+    if(showBosnianDhikrFromStorage){
+      this.showBosnianDhikr = JSON.parse(showBosnianDhikrFromStorage);
+    }
+    else {
+      this.showBosnianDhikr = true;
+    }
+
+    if(showDhikrTranslationFromStorage){
+      this.showDhikrTranslation = JSON.parse(showDhikrTranslationFromStorage);
+    }
+    else {
+      this.showDhikrTranslation = true;
+    }
+  }
+
+  onModelChange(item: DhikrLocalStoarge, value: boolean) {
+    if(item === DhikrLocalStoarge.showArabicDhikr){
+      localStorage.setItem(DhikrLocalStoarge.showArabicDhikr.toString(), JSON.stringify(value));
+    }
+    else if(item === DhikrLocalStoarge.showDhikrTranslation){
+      localStorage.setItem(DhikrLocalStoarge.showDhikrTranslation.toString(), JSON.stringify(value));
+    }
+    else if(item === DhikrLocalStoarge.showBosnianDhikr){
+      localStorage.setItem(DhikrLocalStoarge.showBosnianDhikr.toString(), JSON.stringify(value));
+    }
   }
 }
