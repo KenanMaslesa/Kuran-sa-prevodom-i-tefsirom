9476de8b5f958ef525d24104bab74b5dc40d27cb
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-02-16 15:05:30 +0100

    .

--- src/app/quran/translation/translation.page.ts
+++ src/app/quran/translation/translation.page.ts
@@ -66,7 +66,7 @@ export class TranslationPage implements OnInit {
 
   playAyah(ayahId){
     this.selectedAyah = ayahId;
-    this.mediaPlayerService.playAudio(`https://cdn.islamic.network/quran/audio/64/ar.alafasy/${ayahId}.mp3`);
+    this.mediaPlayerService.playAudio(`https://cdn.islamic.network/quran/audio/128/ar.alafasy/${ayahId}.mp3`);
   }
 
   scrollToBottom(){
