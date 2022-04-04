398fc39cdb77952029ea612c19f1fe720bd85514
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-04-05 01:36:25 +0200

    .

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { PopoverPage } from './components/popover';
 import { LoaderComponent } from './components/loader/loader.component';
 import { PlayerComponent } from './components/player/player.component';
+import { PopoverPage } from './popover';
 @NgModule({
   imports: [IonicModule, FormsModule, CommonModule],
   exports: [LoaderComponent, PlayerComponent, IonicModule, CommonModule,],
