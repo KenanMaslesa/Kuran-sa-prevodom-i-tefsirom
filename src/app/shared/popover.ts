ab460d2ec374eb3ac5b83074a887bc3533eb03aa
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-04-05 03:11:11 +0200

    .

--- src/app/shared/popover.ts
+++ src/app/shared/popover.ts
@@ -11,6 +11,16 @@ import { PopoverController } from '@ionic/angular';
         <ion-icon slot="end" name="information-circle-outline"></ion-icon>
       </ion-item>
 
+      <ion-item button>
+        <ion-label>Podijeli sa drugima</ion-label>
+        <ion-icon slot="end" name="share-social-outline"></ion-icon>
+      </ion-item>
+
+      <ion-item button>
+        <ion-label>Ocijeni nas na Google Play</ion-label>
+        <ion-icon slot="end" name="star-outline"></ion-icon>
+      </ion-item>
+
       <ion-item button (click)="goTo('/tabs/settings')">
         <ion-label>Postavke</ion-label>
         <ion-icon slot="end" name="settings-outline"></ion-icon>
